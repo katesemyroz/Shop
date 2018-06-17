0d65d904d51a5dac4292fd2dd07f5043c649bf2b
Kateryna Semyroz <kateryna.semyroz@gmail.com>
2018-06-17 22:19:49 +0200

    config/database.yml fixed

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180610213800) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "socks", force: :cascade do |t|
     t.string "name"
     t.integer "price"
