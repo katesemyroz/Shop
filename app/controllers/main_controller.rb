1788e1c4bd1db76b2b02f10111b4d994aa7b7ea2
Kateryna Semyroz <kateryna.semyroz@gmail.com>
2018-06-18 03:58:23 +0200

    Style changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    @recent_socks = Sock.paginate(:page => params[:page], :per_page => 3)
+    @recent_socks = Sock.paginate(:page => params[:page], :per_page => 10)
   end
 end
